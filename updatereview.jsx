698e72d06f426698a9db6f92afbfd30f2dc055b5
ashsin12 <84857931+ashsin12@users.noreply.github.com>
2021-10-10 20:21:54 +0530

    Add files via upload

--- updatereview.jsx
+++ updatereview.jsx
@@ -1,54 +1,58 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-class AddReview extends Component {
+class UpdateReview extends Component {
   state = {
     review: {
-              reviewId:0,
-              comment:"",
-              headline:"",
-              rating: "",
-              reviewOn:" ",
-              },
-    books:[]
+      reviewId: "",
+      Comment: "",
+      Headline: "",
+      rating: 0.0,
+    },
   };
+  componentDidMount() {
+    axios
+      .get(
+        `http://localhost:8082/review/${this.props.match.params.reviewId} `
+      )
+      .then((res) => {
+        console.log(res);
+        this.setState({ review: res.data });
+      });
+  }
   handleChange = (e) => {
     const review = { ...this.state.review };
     review[e.target.name] = e.target.value;
     this.setState({ review: review });
-  // this.setState({ review });
+    this.setState({ review });
   };
   handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post(`http://localhost:8082/reviews`, this.state.review)
-      .then(() => {
+      .put(
+        `http://localhost:8082/review/${this.props.match.params.reviewId} `,
+        this.state.review
+      )
+      .then((res) => {
+        console.log(res.data);
+        alert("Updated User successfully!!");
         this.props.history.push("/reviews");
       });
   };
-
-  componentDidMount() {
-    axios
-      .get(`http://localhost:8082/books`)
-      .then((response) => {
-        console.log(response);
-        this.setState({ books:response.data });
-      });
-  }
   render() {
-    //console.log(response);
     return (
       <div>
         <div className="container">
           <div>
             <div className="card col-md-6 offset-md-3 offset-md-3">
-              <h2 className="text-cener">Add Review</h2>
+              <h2 className="text-cener">Update Review</h2>
               <div className="card-body">
                 <form onSubmit={this.handleSubmit}>
+                 
                   <div className="form-group">
                     <label>Comment</label>
                     <input
-                      placeholder="write it"
+                      placeholder="Comment"
                       className="form-control"
                       name="comment"
                       value={this.state.review.comment}
@@ -68,30 +72,15 @@ class AddReview extends Component {
                   <div className="form-group">
                     <label>Rating</label>
                     <input
-                      placeholder="rate it (1 to 5)"
+                      placeholder="rating"
                       className="form-control"
                       name="rating"
                       value={this.state.review.rating}
                       onChange={this.handleChange}
                     />
                   </div>
-                  {/* <div className="form-group">
-                  <label>Title of book</label>
-                  <input 
-                     placeholder="title of book"   
-                     className="form-control"
-                     name="title"
-                     value={this.state.books.title}
-                    //  onChange={(reponse) => this.handleCities(reponse)}
-                    //  {Cities.cities && Cities.cities.map((reponse, key) => {   
-                    //   return <option key={key} value={e.Key}>{e.Value}</option>; })}
-                   />
-                   </div> */}
-  
-                   
-                  
                   <button type="submit" class="btn btn-primary">
-                    Submit
+                    Update
                   </button>
                 </form>
               </div>
@@ -103,15 +92,4 @@ class AddReview extends Component {
   }
 }
 
-export default AddReview;
-
-
-
-
-
-
-
-
-
-
-
+export default UpdateReview;
